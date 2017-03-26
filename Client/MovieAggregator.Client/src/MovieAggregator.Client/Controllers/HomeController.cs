572d319c2c0403d4cbb3ada9d65a5b6595405970
marcobergantin <bergantin.marco@gmail.com>
2017-03-26 13:52:49 +0200

    Added sample api call

--- Client/MovieAggregator.Client/src/MovieAggregator.Client/Controllers/HomeController.cs
+++ Client/MovieAggregator.Client/src/MovieAggregator.Client/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace MovieAggregator.Client.Controllers
 {
@@ -19,9 +20,17 @@ namespace MovieAggregator.Client.Controllers
             }
         }
 
-        public IActionResult Index()
-        {          
-            return View("Index", MovieInfoEndpoint);
+        public async Task<IActionResult> Index()
+        {
+            string resultString = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(MovieInfoEndpoint + "api/Movie?movieTitle=" + "spiderman");
+                response.EnsureSuccessStatusCode();
+                resultString = await response.Content.ReadAsStringAsync();
+            }
+
+            return View("Index", resultString);
         }
     }
 }
