2e9fcb4f29641024d02c75fceffaa0e0ed35832b
marcobergantin <bergantin.marco@gmail.com>
2017-04-01 10:32:38 +0200

    Improved youtube search

---
+++ Server/MovieAggregator.WebApi/VideoProvider.Youtube.ApiInteraction/SearchResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Google.Apis.YouTube.v3.Data;
+using System;
+
+namespace VideoProvider.Youtube.ApiInteraction
+{
+    public static class SearchResultExtensions
+    {
+        public static bool TitleContains(this SearchResult result, string matchString)
+        {
+            if (matchString == null)
+                throw new ArgumentException($"{nameof(matchString)} parameter cannot be null"); 
+
+            if (result == null || result.Snippet == null || result.Snippet.Title == null)
+                return false;
+
+            return result.Snippet.Title.IndexOf(matchString, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+}
--- Server/MovieAggregator.WebApi/VideoProvider.Youtube.ApiInteraction/YoutubeVideoProvider.cs
+++ Server/MovieAggregator.WebApi/VideoProvider.Youtube.ApiInteraction/YoutubeVideoProvider.cs
@@ -6,48 +6,63 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Linq;
 using System;
+using Google.Apis.YouTube.v3.Data;
+using System.Collections.Generic;
 
 namespace VideoProvider.Youtube.ApiInteraction
 {
     public class YoutubeVideoProvider : ITrailerProvider
     {
+        const string DefaultSearchResultProperty = "snippet";
+        const string SearchResultType = "video";
+        const string TrailerVideoCategoryId = "44";
+
+        const int MaxResultsPerRequest = 10;
+
         private static readonly string ClientID = ConfigurationManager.AppSettings["GoogleApisClientID"];
         private static readonly string ClientSecret = ConfigurationManager.AppSettings["GoogleApisClientSecret"];
         private static YouTubeService youtubeService;
 
+        private static YouTubeService YouTubeService
+        {
+            get
+            {
+                return youtubeService ?? (youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                                                {
+                                                    ApiKey = ClientSecret,
+                                                    ApplicationName = ClientID
+                                                }
+                                         ));
+            }
+        }
+
         public async Task<MovieTrailerDTO> GetTrailer(string movieTitle, DateTime? releaseDate)
         {
             //let's include all the movies from the beginning of cinema to unreleased with a 5 year scope
-            if (releaseDate.HasValue && (releaseDate.Value.Year < 1890 || releaseDate.Value > DateTime.Now.AddYears(5)))
+            if (ValidateReleaseDate(releaseDate) == false)
                 throw new ArgumentException("ReleaseDate is not valid");
 
-            return await SearchVideo(movieTitle + " official trailer " + (releaseDate.HasValue ? releaseDate.Value.Year.ToString() : ""), releaseDate);
+            return await SearchVideo(movieTitle, releaseDate);
+        }
+
+        private bool ValidateReleaseDate(DateTime? releaseDate)
+        {
+            //let's include all the movies from the beginning of cinema to unreleased with a 5 year scope
+            return (releaseDate.HasValue && (releaseDate.Value.Year < 1890 || releaseDate.Value > DateTime.Now.AddYears(5)));
         }
 
         public async Task<MovieTrailerDTO> SearchVideo(string searchQuery, DateTime? releaseDate)
         {
-            if (youtubeService == null)
-            {
-                youtubeService = new YouTubeService(new BaseClientService.Initializer()
-                {
-                    ApiKey = ClientSecret,
-                    ApplicationName = ClientID
-                });
-            }
-
             var searchListRequest = BuildRequest(searchQuery, releaseDate);
             var searchListResponse = await searchListRequest.ExecuteAsync();
             if (searchListResponse != null && searchListResponse.Items != null)
             {
-                var searchResult = searchListResponse.Items.FirstOrDefault();
+                var searchResult = FilterSearchResults(searchQuery,
+                                                       (releaseDate.HasValue ? releaseDate.Value.Year.ToString() : null), 
+                                                       searchListResponse.Items);
                 if (searchResult != null)
                 {
-                    return new MovieTrailerDTO
-                    {
-                        VideoTitle = searchResult.Snippet.Title,
-                        VideoURL = $"https://www.youtube.com/watch?v={searchResult.Id.VideoId}",
-                        EmbedURL = $"https://www.youtube.com/embed/{searchResult.Id.VideoId}"
-                    };
+                    return MapToDTO(searchResult);
                 }
             }
 
@@ -56,12 +71,12 @@ namespace VideoProvider.Youtube.ApiInteraction
 
         private SearchResource.ListRequest BuildRequest(string searchString, DateTime? releaseDate)
         {
-            var searchListRequest = youtubeService.Search.List("snippet");
+            var searchListRequest = YouTubeService.Search.List(DefaultSearchResultProperty);
             searchListRequest.Q = searchString;
-            searchListRequest.MaxResults = 1;           
+            searchListRequest.MaxResults = MaxResultsPerRequest;
             searchListRequest.Order = SearchResource.ListRequest.OrderEnum.Relevance;
-            searchListRequest.Type = "video";
-            searchListRequest.VideoCategoryId = "44"; //Trailers
+            searchListRequest.Type = SearchResultType;
+            searchListRequest.VideoCategoryId = TrailerVideoCategoryId;
             searchListRequest.VideoDuration = SearchResource.ListRequest.VideoDurationEnum.Short__;
             searchListRequest.VideoEmbeddable = SearchResource.ListRequest.VideoEmbeddableEnum.True__;
             //trailers usually come out in the year prior the release of the movie
@@ -72,6 +87,42 @@ namespace VideoProvider.Youtube.ApiInteraction
                 searchListRequest.PublishedAfter = releaseDate.Value.Subtract(new TimeSpan(365, 0, 0, 0));
             }
             return searchListRequest;
+        }
+
+        private SearchResult FilterSearchResults(string movieTitle, string yearString, IList<SearchResult> results)
+        {
+            var subset = results.Where(r => r.TitleContains(movieTitle));
+            if (subset == null || subset.Count() == 0)
+            {
+                subset = results;
+            }
+
+            if (string.IsNullOrWhiteSpace(yearString) == false)
+            {
+                var yearInTitleSubset = subset.Where(r => r.Snippet.Title.Contains(yearString));
+                if (yearInTitleSubset != null && yearInTitleSubset.Count() > 0)
+                {
+                    subset = yearInTitleSubset;
+                }
+
+                var yearInDescriptionTooSubset = subset.Where(r => r.Snippet.Description.Contains(yearString));
+                if (yearInDescriptionTooSubset != null && yearInDescriptionTooSubset.Count() > 0)
+                {
+                    subset = yearInDescriptionTooSubset;
+                }
+            }
+
+            return subset.FirstOrDefault();
+        }
+
+        private MovieTrailerDTO MapToDTO(SearchResult searchResult)
+        {
+            return new MovieTrailerDTO
+            {
+                VideoTitle = searchResult.Snippet.Title,
+                VideoURL = $"https://www.youtube.com/watch?v={searchResult.Id.VideoId}",
+                EmbedURL = $"https://www.youtube.com/embed/{searchResult.Id.VideoId}"
+            };
         }
     }
 }
